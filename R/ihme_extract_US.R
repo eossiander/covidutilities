291835d73ac19b4b9536fe25ebbd791381282430
Eric Ossiander <ericossiander@gmail.com>
2020-05-12 22:08:47 -0700

    added readme file

--- R/ihme_extract_US.R
+++ R/ihme_extract_US.R
@@ -13,10 +13,11 @@ ihme_extractUS <- function(dat){
     dat %>%
         dplyr::rename(state = location_name) %>%
         dplyr::filter(state %in% .US_states) %>%
-        dplyr::select(state, date, allbed_mean, allbed_lower, allbed_upper, ICUbed_mean,
-               ICUbed_lower, ICUbed_upper, InvVen_mean, InvVen_lower, InvVen_upper,
-               deaths_mean, deaths_lower, deaths_upper, admis_mean, admis_lower,
-               admis_upper, newICU_mean, newICU_lower, newICU_upper, totdea_mean,
-               totdea_lower, totdea_upper, bedover_mean, bedover_lower, bedover_upper,
-               icuover_mean, icuover_lower, icuover_upper, projection_date)
+        dplyr::select_if(!(names(.) %in% c('V1')))
+        ## dplyr::select(state, date, allbed_mean, allbed_lower, allbed_upper, ICUbed_mean,
+        ##        ICUbed_lower, ICUbed_upper, InvVen_mean, InvVen_lower, InvVen_upper,
+        ##        deaths_mean, deaths_lower, deaths_upper, admis_mean, admis_lower,
+        ##        admis_upper, newICU_mean, newICU_lower, newICU_upper, totdea_mean,
+        ##        totdea_lower, totdea_upper, bedover_mean, bedover_lower, bedover_upper,
+        ##        icuover_mean, icuover_lower, icuover_upper, projection_date)
 }
